5ee5569a51bdd0cbf77f87867b56f3b04dba066a
rcMcQueen <schroener1@gmail.com>
2016-11-21 12:52:38 -0800

    reverted to old version

--- views/js/attractionsAndRoutes2start.js
+++ views/js/attractionsAndRoutes2start.js
@@ -4,37 +4,45 @@ script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 function getPopularAttractions() {
-     $(document).ready(function () {
-         $.ajax({
-             type: 'GET',
-             url: 'http://159.203.47.53:8080/popAttr',
-             dataType: 'json',
-             success: function (data) {
-                 data = JSON.stringify(data);
-                 console.log(data);
-             },
-             error: function (data) {
-                 console.log('Error, Ajax call unsuccessful.', data);
-             }
-         });
-     });
+    var DATA = {};
+    $(document).ready(function () {
+        $.ajax({
+            type: 'GET',
+            url: 'http://159.203.47.53:8080/popAttr',
+            dataType: 'json',
+            success: function (data) {
+                DATA = data;
+                data = JSON.stringify(data);
+                console.log(data);
+            },
+            error: function (err) {
+                DATA = err;
+                console.log('Error, Ajax call unsuccessful.', err);
+            }
+        });
+    });
+    return DATA;
  };
 
 function getRecommendedRoutes() {
+    var DATA = {};
      $(document).ready(function () {
          $.ajax({
              type: 'GET',
              url: 'http://159.203.47.53:8080/recRoute',
              dataType: 'json',
              success: function (data) {
+                 DATA = data;
                  data = JSON.stringify(data);
                  console.log(data);
              },
-             error: function (data) {
-                 console.log('Error, Ajax call unsuccessful.', data);
+             error: function (err) {
+                 DATA = err;
+                 console.log('Error, Ajax call unsuccessful.', err);
              }
          });
      });
+    return DATA;
  };
 
 function updateAttractions() {
@@ -62,6 +70,7 @@ function updateAttractions() {
 
 function getAttractions() {
      var attrTypes = {};
+     var DATA = {};
      attrTypes = updateAttractions();
      $(document).ready(function () {
          $.ajax({
@@ -70,12 +79,15 @@ function getAttractions() {
              url: 'http://159.203.47.53:8080/makeAttr',
              dataType: 'json',
              success: function (data) {
+                 DATA = data;
                  data = JSON.stringify(data);
                  console.log(data);
              },
              error: function (err) {
+                 DATA = err;
                  console.log('Error, Ajax call unsuccessful.', err);
              }
          });
      });
+    return DATA;
 };
