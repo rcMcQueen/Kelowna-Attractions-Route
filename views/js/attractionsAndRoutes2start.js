42ec4ab1a9a6862c89e6ca2761b10315219ae975
Ryan Kramer <kramer545@gmail.com>
2016-11-21 13:34:29 -0800

    Add files via upload

--- views/js/attractionsAndRoutes2start.js
+++ views/js/attractionsAndRoutes2start.js
@@ -1,6 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js';
-script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 function getPopularAttractions() {
