03112103a9b3458b198811b058a5dfcb2a5dd65d
Ryan Kramer <kramer545@gmail.com>
2016-11-23 12:10:31 -0800

    changed data = json to DATA = json, as function returns DATA

--- views/js/attractionsAndRoutes2start.js
+++ views/js/attractionsAndRoutes2start.js
@@ -1,3 +1,5 @@
+document.getElementsByTagName('head')[0].appendChild(script);
+
 function getPopularAttractions() {
     var DATA = {};
     $(document).ready(function () {
@@ -28,7 +30,7 @@ function getRecommendedRoutes() {
              dataType: 'json',
              success: function (data) {
                  DATA = data;
-                 data = JSON.stringify(data);
+                 DATA = JSON.stringify(data);
                  console.log(data);
              },
              error: function (err) {
